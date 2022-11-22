d4ab988df961734e6d576995073f36f04f1b5148
kobezzza <kobezzza@gmail.com>
2022-11-22 17:48:02 +0300

    feat: added support for `ssr`

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ exports.adapter = function (txt, opt_params, opt_info) {
 };
 
 const
-	renderFnRgxp = /render\s*\(.*?\)\s*{/,
-	importDeclRgxp = /\bimport\s+{([^}]*)}\s+from\s+(['"])vue\2/g,
+	renderFnRgxp = /(?:ssrR|r)ender\s*\((.*?)\)\s*{/,
+	importDeclRgxp = /\bimport\s+{([^}]*)}\s+from\s+(['"])(.*?)\2/g,
 	exportDeclRgxp = /\bexport\s+/g;
 
 const hoistedElementsRgxp =
@@ -28,18 +28,15 @@ const hoistedPropsRgxp =
 	/(_hoisted_\d+)\s*=\s*(\{[\s\S]+?})(?=\s*const |\s*function )/g;
 
 function template(id, fn, txt, p) {
-	let code = sfc.compileTemplate({
-		id,
-		source: txt,
-		...p
-	}).code;
+	let
+		{code} = sfc.compileTemplate({id, ...p, source: txt});
 
 	const
+		args = renderFnRgxp.exec(code)[1],
 		vars = [];
 
 	code = code
 		.replace(renderFnRgxp, 'render() {')
-
 		.replace(exportDeclRgxp, '')
 
 		.replace(hoistedElementsRgxp, (_, id, decl) =>
@@ -48,30 +45,33 @@ function template(id, fn, txt, p) {
 		.replace(hoistedPropsRgxp, (_, id, decl) =>
 			`${id} = _ctx.$renderEngine.r.resolveAttrs.call(_ctx, {props: ${decl}}).props\n`)
 
-		.replace(importDeclRgxp, (_, decl) => {
+		.replace(importDeclRgxp, (_, decl, q, lib) => {
 			decl = decl.replace(/\sas\s/g, ': ');
 
-			decl.split(/\s*,\s*/).forEach((varDecl) => {
-				const v = varDecl.split(/\s*:\s*/);
-				vars.push((v[1] ?? v[0]).trim());
-			});
+			if (lib === 'vue') {
+				decl.split(/\s*,\s*/).forEach((varDecl) => {
+					const v = varDecl.split(/\s*:\s*/);
+					vars.push((v[1] ?? v[0]).trim());
+				});
 
-			return `const {${decl}} = _ctx.$renderEngine.r;`;
+				return `const {${decl}} = _ctx.$renderEngine.r;`;
+			}
+
+			return `const {${decl}} = require('${lib}');`;
 		});
 
-	const
-		renderMethodsRgxp = new RegExp(`\\b(${vars.join('|')})\\s*[(]`, 'g');
+	if (vars.length > 0) {
+		const renderMethodsRgxp = new RegExp(`\\b(${vars.join('|')})\\s*[(]`, 'g');
+		code = code.replace(renderMethodsRgxp, (_, $1) => `${$1}.call(_ctx,`);
+	}
 
-	code = code
-		.replace(renderMethodsRgxp, (_, $1) => `${$1}.call(_ctx,`);
-
-	return `${id} = ${fn}return ${toFunction(`${code}; return render;`)}};`;
+	return `${id} = ${fn}return ${toFunction(args, `${code}; return render;`)}};`;
 }
 
 function setParams(p) {
 	return {...p};
 }
 
-function toFunction(code) {
-	return `function (_ctx, _cache) {${code}}`;
+function toFunction(args, code) {
+	return `function (${args}) {${code}}`;
 }
